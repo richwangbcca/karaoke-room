eb85f5172b5d93487dc2c85cfa58b1a0498f6394
richwangbcca <richwangbcca@gmail.com>
2026-08-26 21:33:53 -0700

    Clean up landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -55,8 +55,8 @@ function App() {
   if (!role) {
     return (
       <div className="home">
-        <h1>Karaoke Room 🎤</h1>
-        <h2>Join a room, add your songs, and sing together!</h2>
+        <h1>Karaoke Room</h1>
+        <h2>Join a room, add your songs, and sing together</h2>
         <div className="form">
           <div className="input-box">
             <label>Name</label>
